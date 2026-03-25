8cb18df7570bf083ec671a5914e38ccfd79ab0f8
MoganaMurali D <speeddroners@gmail.com>
2026-03-25 19:56:55 +0530

    The basic login program

---
+++ Logged.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Login {
+    String username;
+    String password;
+
+    Login(String u, String p) {
+        username = u;
+        password = p;
+    }
+
+    void checker(String u, String p) {
+        if (u.equals(username) && p.equals(password)) {
+            System.out.println("Login successful brothaa");
+        } else {
+            System.out.println("Invalid credentials");
+        }
+    }
+}
+
+public class Logged {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Login lg = new Login("speeddroners", "twsl21");
+
+        System.out.print("Enter the username: ");
+        String u = sc.next();
+
+        System.out.print("Enter the password: ");
+        String p = sc.next();
+
+        lg.checker(u, p);
+
+        sc.close();
+    }
+}
