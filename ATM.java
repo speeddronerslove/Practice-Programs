f77619d6e87f1964a106341fb12c044bf4ff02a4
MoganaMurali D <speeddroners@gmail.com>
2026-03-25 19:20:02 +0530

    ATM CODE FINISHED

--- ATM.java
+++ ATM.java
@@ -10,33 +10,40 @@ class Bankaccount {
     }
 
     void deposit (int amount) {
-        System.out.println ("The balance before deposit"+balance);
-        amount += balance;
-        System.out.println("The balance after the deposit"+balance);
+        System.out.println ("The balance before deposit "+balance);
+        balance += amount;
+        System.out.println("The balance after the deposit "+balance);
 
     }
 
     void withdraw (int amount) {
+        if (amount <= balance){
         System.out.println("The amount before withdraw "+balance);
         balance -= amount ;
-        System.out.println("Balance after withdraw"+balance);
+        System.out.println("Balance after withdraw "+balance);}
+        else{
+            System.out.println(" Insufficient balance ");
+        }
 
     }
     
 }
 public class ATM {
+    public static void main (String args []){
     Scanner sc = new Scanner (System.in);
     Bankaccount Bnk = new Bankaccount("Mogana", 1000);
     while (true){
-    System.out.println("Enter the choices 1.Deposit\r\n" + //
+    System.out.println("Enter the choices\r\n"+  "1.Deposit\r\n" + //
                 "2.Withdraw\r\n" + //
                 "3.Exit");
     int choice = sc.nextInt();
     if (choice==1){
+        System.out.println("Enter the amount to be Deposited");
         int amount = sc.nextInt();
         Bnk.deposit(amount);
     }
     else if (choice==2){
+        System.out.println("Enter the amount to be withdrawed");
         int amount = sc.nextInt();
         Bnk.withdraw(amount);
         
@@ -47,4 +54,5 @@ public class ATM {
 
     }
     }
+}
 }
