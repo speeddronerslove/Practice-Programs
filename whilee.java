4240a802c5563bf49ff12cb4019658ababbd42da
MoganaMurali D <speeddroners@gmail.com>
2026-04-14 22:38:59 +0530

    whilee

---
+++ whilee.java
@@ -0,0 +1,9 @@
+public class whilee {
+    public static void main (String args []){
+        int i = 10 ;
+         while (i < 100){
+            System.out.println(i);
+            i++;
+         }
+    }
+}
