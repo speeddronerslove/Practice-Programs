5134431a2192303677a07417e357bb903e0795c4
MoganaMurali D <speeddroners@gmail.com>
2026-05-06 22:15:00 +0530

    revwords

---
+++ revwords.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class revwords{
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a String = ");
+            String str = sc.nextLine();
+            String [] words = str.split(" ");
+            for (int i= words.length-1;i>=0;i--){
+                System.out.println(words[i]+" ");
+            }
+sc.close();
+        }
+}
