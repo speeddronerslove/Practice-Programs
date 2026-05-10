939157ddb537c9ed8935f6b4bf8891995565aa26
MoganaMurali D <speeddroners@gmail.com>
2026-05-10 22:06:39 +0530

    Haspmap 3

---
+++ Hashmap3.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+public class Hashmap3 {
+    public static void main(String[] args) {
+        HashMap <String,Integer> hm = new HashMap <> ();
+        hm.put("Murali",80);
+        hm.put("Dolli",100);
+        System.out.println(hm);
+    }
+}
