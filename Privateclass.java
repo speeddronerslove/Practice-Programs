6e26c5290770beed79f1798d6ae47cf361b4256c
MoganaMurali D <speeddroners@gmail.com>
2026-06-04 17:04:31 +0530

    private

---
+++ Privateclass.java
@@ -0,0 +1,12 @@
+class Privateclass {
+    String name = "She <3";
+    private int age = 21;
+    private String gender = "Female";
+    public static void main(String[] args) {
+        Privateclass oso = new Privateclass();
+        System.out.println(oso.name);
+        System.out.println(oso.gender);
+        System.out.println(oso.age);
+
+    }
+}
