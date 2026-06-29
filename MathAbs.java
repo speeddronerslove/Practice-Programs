5ead242e54d9e5947d356a05e13bf1348babfc5c
MOGANA MURALI D <speeddroners@gmail.com>
2026-06-29 23:31:52 +0530

    Add files via upload

---
+++ MathAbs.java
@@ -0,0 +1,8 @@
+public class MathAbs{
+    public static void main(String[] args){
+        System.out.println("the value of -4.5:" + Math.abs(-4.5));
+        System.out.println("the value of 4.5:" + Math.abs(4.5));
+        System.out.println("the value of 3:" + Math.abs(3));
+    }
+
+}
